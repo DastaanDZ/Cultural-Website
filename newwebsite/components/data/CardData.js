6c039c64e859751804c58ac68cbc30a0519f8b23
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2022-06-02 13:41:50 +0530

    ready to deploy

---
+++ newwebsite/components/data/CardData.js
@@ -0,0 +1,29 @@
+import img from './one.jpg'
+import img2 from './two.jpg'
+import img3 from './three.jpg'
+import img4 from './four.jpg'
+import img5 from './five.jpg'
+const CardData = [
+    {
+        id:'1',
+        pic: img
+    },
+    {
+        id:'2',
+        pic: img2
+    },
+    {
+        id:'3',
+        pic: img3
+    },
+    {
+        id:'4',
+        pic: img4
+    },
+    {
+        id:'5',
+        pic: img5
+    }
+]
+
+export default CardData
